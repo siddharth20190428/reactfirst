49ff61da6d42b1e295916b15a11d21eace2ffc7e
Siddharth Sahu <50068508+siddharth20190428@users.noreply.github.com>
2020-09-02 17:05:18 +0530

    Update Common.jsx

--- src/Navbar/Common.jsx
+++ src/Navbar/Common.jsx
@@ -12,7 +12,7 @@ const Common = (props) => {
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
                   <h1>
                     {props.name}
-                    <strong className="brand-name"> Code: To Core</strong>
+                    <strong className="brand-name"> Code : To Core</strong>
                   </h1>
                   <h2 className="my-3">
                     We are the team of talented developer making websites
